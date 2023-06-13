5491a69d578b213fabc834d60df8c06a4454eec2
Syed Abdul Raheem <abdulraheem.s@holosuit.com>
2023-06-14 01:52:13 +0530

    Palindrom Question Solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,27 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
-
+function RemoveSpecialCharacter(str) {
+  for (let i of str) {
+    if (!(i >='a' && i <='z')) 
+    {
+      str = str.replace(i, "");
+    }
+  }
+  return str;
+}
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  str = RemoveSpecialCharacter(str);
+  var i = 0;
+  var j = str.length - 1;
+  while (i <= j) {
+    if (str[i] != str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
   return true;
 }
-
 module.exports = isPalindrome;
